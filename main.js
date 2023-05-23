cf88e1967cb802fc6f3f12a08143e593cf926467
IsraelaZimru <slzimro@gmail.com>
2023-05-23 04:44:06 +0300

    Using dynamic  data

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+
+
+// importing data for stories
+let data = undefined;
+fetch("./fakeData.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(([dbData]) => {
+        data = dbData;
+        console.log(data);
+        new SlideStories("slide");
+    })
+
+
+// -------------------------------------------------------------------------------
 class SlideStories {
     constructor(id) {
         this.slide = document.querySelector(`[data-slide=${id}]`);
@@ -16,7 +32,7 @@ class SlideStories {
         });
         this.thumbItems[index].classList.add("active");
         this.autoSlide();
-        console.log(this.thumbItems.classList);
+        // console.log(this.thumbItems.classList);
     }
 
     prev() {
@@ -53,6 +69,15 @@ class SlideStories {
     }
 
     init() {
+        //adding new slides elements:
+        const HTMLcontent = data.stories.map(story => NewElement("img", [["src", story.img.src || ""], ["alt", story.img.alt || ""]]))
+        const slideContainer = document.getElementById("slideContainer");
+        slideContainer.append(...HTMLcontent)
+        // console.log(HTMLcontent);
+
+
+
+
         this.next = this.next.bind(this);
         this.prev = this.prev.bind(this);
         this.items = this.slide.querySelectorAll(".slide-items > *");
@@ -63,4 +88,20 @@ class SlideStories {
     }
 }
 
-new SlideStories("slide");
+
+// ------------------------------------------------
+// helperFunctions:
+
+
+function NewElement(type, attributeList = [], cssClassLst = []) {
+    const temp = document.createElement(type);
+
+    temp.classList.add(...cssClassLst)
+
+    for (let i = 0; i < attributeList.length; i++) {
+        const attribute = attributeList[i];
+        temp.setAttribute(...attribute);
+    }
+    return temp
+}
+
